518749d963b63b1354d31f6fdb92ffde9e18f92f
112310221 <146065074+112310221@users.noreply.github.com>
2023-10-31 14:50:31 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -3,22 +3,8 @@
 
 int main()
 {
-    int h,m;
-    float hA,mA,A;
-    scanf("%d,%d",&h,&m);
-    hA=h*30.0+(30.0/60.0*m);
-    mA=6.0*m;
-    if(hA>mA){
-        A=hA-mA;
-    }
-    else if(hA<=mA){
-        A=mA-hA;
-    }
-    if(A>180){
-        A=360-A;
-    }
-    printf("%.3f",A);
-
-
-
+    float C,Y;
+    scanf("%f",&C);
+    Y=((C*9.0)/5.0)+32.0;
+    printf("%.1f",Y+0.05);
 }
