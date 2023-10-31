f0cdf7ee7b086e058fb8de90f1da189d726291f2
112310221 <146065074+112310221@users.noreply.github.com>
2023-10-31 14:47:21 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int min;
+    float num;
+    scanf("%d",&min);
+    if(min<=800){
+            num=min*0.9;
+            printf("%.1f",num);
+    }
+    else if(min>=800 && min<=1500){
+            num=min*0.9*0.9;
+            printf("%.1f",num);
+    }
+    else if(min>=1500){
+            num=min*0.9*0.79;
+            printf("%.1f",num);
+    }
+
+}
 
